8e44641c5b7b565fa5e1ef7b2c8bbf8251afcbd9
hoth6615 <73147459+hoth6615@users.noreply.github.com>
2020-11-25 18:00:56 +0900

    11.25_park_3

--- PetDiary/app/src/main/java/com/example/petdiary/adapter/ViewPageAdapter.java
+++ PetDiary/app/src/main/java/com/example/petdiary/adapter/ViewPageAdapter.java
@@ -1,12 +1,7 @@
 package com.example.petdiary.adapter;
 
 import android.content.Context;
-<<<<<<< HEAD
-import android.graphics.Color;
-import android.util.Log;
-=======
 import android.content.Intent;
->>>>>>> d30bd908c010dde8dbd4adf94693ab6f2c40db89
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,9 +61,7 @@ public class ViewPageAdapter extends PagerAdapter {
                 (Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.slider, container, false);
         ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
-<<<<<<< HEAD
         Glide.with(context).load(images.get(position)).centerCrop().override(1000).into(imageView);
-=======
 
         v.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -77,9 +70,6 @@ public class ViewPageAdapter extends PagerAdapter {
             }
         });
 
-        //imageView.setImageResource(images[position]);
-        Glide.with(context).load(imagess.get(position)).into(imageView);
->>>>>>> d30bd908c010dde8dbd4adf94693ab6f2c40db89
         container.addView(v);
         return v;
     }
