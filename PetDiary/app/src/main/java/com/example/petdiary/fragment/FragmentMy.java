f88b7f658e4287be29e5e3c1b88cf2eef7081635
myocon <myo_con@naver.com>
2020-11-27 02:37:46 +0900

    mypage - 새로고침추가 (미완)

--- PetDiary/app/src/main/java/com/example/petdiary/fragment/FragmentMy.java
+++ PetDiary/app/src/main/java/com/example/petdiary/fragment/FragmentMy.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,6 +19,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.petdiary.Data;
 import com.example.petdiary.Kon_MypageAdapter;
@@ -29,6 +31,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -48,6 +51,8 @@ import static android.app.Activity.RESULT_OK;
 public class FragmentMy extends Fragment {
 
     private static final String TAG = "MyPage_Fragment";
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private BottomNavigationView bottomNavigationView;
 
     TextView profileName;
     TextView profileMemo;
@@ -55,6 +60,7 @@ public class FragmentMy extends Fragment {
 
     Map<String, String> userInfo = new HashMap<>();
     ArrayList<Data> postList = new ArrayList<Data>();
+    int listCount = 0;
 
     ImageView profileEditImg;
     ViewGroup viewGroup;
@@ -73,6 +79,7 @@ public class FragmentMy extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         viewGroup = (ViewGroup) inflater.inflate(R.layout.kon_fragment_mypage, container, false);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) viewGroup.findViewById(R.id.swipe_layout);
         profileEditImg = viewGroup.findViewById(R.id.profile_image);
         profileName = viewGroup.findViewById(R.id.profile_name);
         profileMemo = viewGroup.findViewById(R.id.profile_memo);
@@ -113,7 +120,7 @@ public class FragmentMy extends Fragment {
         });
 
         //////////////////////////////////// 게시물 로드
-        loadPosts();
+        loadPostsAfterCheck(false);
 
         //////////////////////////////////// 리사이클러뷰 setting
         recyclerView = (RecyclerView) viewGroup.findViewById(R.id.recyclerView);
@@ -125,9 +132,8 @@ public class FragmentMy extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
 
         // 리사이클러뷰 간격추가
-        RecyclerDecoration spaceDecoration = new RecyclerDecoration(10,20);
+        RecyclerDecoration spaceDecoration = new RecyclerDecoration(10, 20);
         recyclerView.addItemDecoration(spaceDecoration);
-
 
 
         //////////////////////////////////// 프로필 이미지 수정
@@ -159,13 +165,37 @@ public class FragmentMy extends Fragment {
             }
         });
 
+        // 새로고침시 업데이트
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                postList.clear();
+                loadPostsAfterCheck(false);
+                // 동글동글 도는거 사라짐
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
         return viewGroup;
     }
 
+    //////////////////////////////////// 화면 활성화/비활성화 시 행동
 
-    // 개인 게시물 로드
-    private void loadPosts()
-    {
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (hidden) {
+            Log.d("히든체크!! : ", "onHiddenChanged: " + hidden);
+        } else {
+            Log.d("히든체크!! 보임 : ", "onHiddenChanged: " + hidden);
+            loadPostsAfterCheck(true);
+            moveTop();
+        }
+    }
+
+
+    // 개인 게시물 로드. 체크하게 되면 이전 게시물 개수와 비교후 업데이트, 체크 안하면 그냥 업데이트
+    private void loadPostsAfterCheck(final boolean needCheck) {
         //  유저
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
@@ -174,30 +204,34 @@ public class FragmentMy extends Fragment {
         Query query = db.collection("post").whereEqualTo("uid", uid);
         //query.get
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            Log.d("몇개의 아이템이 있나요 ? : ", "onComplete: " + task.getResult().size());
-                            for (QueryDocumentSnapshot document : task.getResult()) {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    int resultCount = task.getResult().size();
+                    if (needCheck)
+                        if (listCount == resultCount)
+                            return;
 
-                                Data dataList = new Data();
-                                dataList.setUid(document.getData().get("uid").toString());
-                                dataList.setContent(document.getData().get("content").toString());
-                                dataList.setImageUrl1(document.getData().get("imageUrl1").toString());
-                                dataList.setImageUrl2(document.getData().get("imageUrl2").toString());
-                                dataList.setImageUrl3(document.getData().get("imageUrl3").toString());
-                                dataList.setImageUrl4(document.getData().get("imageUrl4").toString());
-                                dataList.setImageUrl5(document.getData().get("imageUrl5").toString());
-                                dataList.setNickName(document.getData().get("nickName").toString());
-                                postList.add(0, dataList);
-                                Log.d("태그 확인", document.getId() + "dataList => " + dataList.getImageUrl1());
-                            }
-                            adapter.notifyDataSetChanged();
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                        Data dataList = new Data();
+                        dataList.setUid(document.getData().get("uid").toString());
+                        dataList.setContent(document.getData().get("content").toString());
+                        dataList.setImageUrl1(document.getData().get("imageUrl1").toString());
+                        dataList.setImageUrl2(document.getData().get("imageUrl2").toString());
+                        dataList.setImageUrl3(document.getData().get("imageUrl3").toString());
+                        dataList.setImageUrl4(document.getData().get("imageUrl4").toString());
+                        dataList.setImageUrl5(document.getData().get("imageUrl5").toString());
+                        dataList.setNickName(document.getData().get("nickName").toString());
+                        postList.add(0, dataList);
                     }
-                });
+                    adapter.notifyDataSetChanged();
+                    listCount = resultCount;
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
 
     }
 
@@ -208,7 +242,7 @@ public class FragmentMy extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            case 0:
+            case 0:   // ProfileEditActivity에서 받아온 값
                 if (resultCode == RESULT_OK) {
                     setProfileImg(data.getStringExtra("profileImg"));
                     profileName.setText(data.getStringExtra("nickName"));
@@ -243,6 +277,18 @@ public class FragmentMy extends Fragment {
 
     private void setProfileImg(String profileImg) {
         Glide.with(this).load(profileImg).centerCrop().override(500).into(profileEditImg);
+    }
+    private void moveTop(){
+        bottomNavigationView = getActivity().findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                Log.d("마지막 로그 확인 ", "onNavigationItemReselected: " + menuItem.getItemId());
+                if(menuItem.getItemId() == R.id.tab4){
+                    recyclerView.smoothScrollToPosition(0);
+                }
+            }
+        });
     }
 
 }
