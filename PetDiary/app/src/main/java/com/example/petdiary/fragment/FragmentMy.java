dfb223ac17328d923d1a8a5544d648fe45ea08a3
myocon <myo_con@naver.com>
2020-11-26 21:22:52 +0900

    mypage - 추가한 게시물 이미지 보이기
    기능 추가 예정

--- PetDiary/app/src/main/java/com/example/petdiary/fragment/FragmentMy.java
+++ PetDiary/app/src/main/java/com/example/petdiary/fragment/FragmentMy.java
@@ -2,44 +2,43 @@ package com.example.petdiary.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.petdiary.Data;
+import com.example.petdiary.Kon_MypageAdapter;
+import com.example.petdiary.RecyclerDecoration;
 import com.example.petdiary.activity.*;
 import com.bumptech.glide.Glide;
 import com.example.petdiary.R;
-import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,61 +51,41 @@ public class FragmentMy extends Fragment {
 
     TextView profileName;
     TextView profileMemo;
-    String profileImgName;   // 프로필이 로딩되면 null값이 아니게 됨
+    String profileImgName;
 
     Map<String, String> userInfo = new HashMap<>();
-
-
+    ArrayList<Data> postList = new ArrayList<Data>();
 
     ImageView profileEditImg;
-    int[] imgs = {
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-            R.drawable.cat1,
-            R.drawable.cat2,
-            R.drawable.dog,
-    };
-
-
-    //    TextView emailTextView;
-//    TextView nickNameTextView;
-//    TextView toolbarNickName;
-//    ImageView user_profileImage_ImageView;
     ViewGroup viewGroup;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    // 리사이클뷰에 필요한거
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_mypage_kon, container, false);
 
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.kon_fragment_mypage, container, false);
         profileEditImg = viewGroup.findViewById(R.id.profile_image);
         profileName = viewGroup.findViewById(R.id.profile_name);
         profileMemo = viewGroup.findViewById(R.id.profile_memo);
+
+        ImageView petAddBtn = viewGroup.findViewById(R.id.profile_petAddBtn);
+        final ImageView profileImage = viewGroup.findViewById(R.id.profile_image);
+
 
         //  유저
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
-
-        // 프로필 이미지, 닉네임, 메모 가져오기,
+        //////////////////////////////////// 프로필 이미지, 닉네임, 메모 가져오기,
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
 
@@ -116,10 +95,10 @@ public class FragmentMy extends Fragment {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     //userInfo.put("email",document.getString("email"));
-                    userInfo.put("nickName",document.getString("nickName"));
+                    userInfo.put("nickName", document.getString("nickName"));
                     //userInfo.put("password",document.getString("password"));
-                    userInfo.put("profileImg",document.getString("profileImg"));
-                    userInfo.put("memo",document.getString("memo"));
+                    userInfo.put("profileImg", document.getString("profileImg"));
+                    userInfo.put("memo", document.getString("memo"));
 
 
                     profileName.setText(userInfo.get(("nickName")));
@@ -133,39 +112,29 @@ public class FragmentMy extends Fragment {
             }
         });
 
+        //////////////////////////////////// 게시물 로드
+        loadPosts();
 
-        ////////////// irang Start
-        GridView gridView = viewGroup.findViewById(R.id.gridView);
-        GridListAdapter adapter = new GridListAdapter();
-        gridView.setAdapter(adapter);
+        //////////////////////////////////// 리사이클러뷰 setting
+        recyclerView = (RecyclerView) viewGroup.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
 
-        for (int i = 0; i < imgs.length; ++i) {
-            adapter.addItem(new MypageGridItem(imgs[i]));
-        }
+        adapter = new Kon_MypageAdapter(postList, getContext());
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+        layoutManager = new GridLayoutManager(getContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
 
+        // 리사이클러뷰 간격추가
+        RecyclerDecoration spaceDecoration = new RecyclerDecoration(10,20);
+        recyclerView.addItemDecoration(spaceDecoration);
 
-        ImageView addPetBtn = viewGroup.findViewById(R.id.profile_petAddBtn);
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getContext(), position + "번째 아이템이 클릭되었다!!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        // 프로필뷰 관련코드.
-        ImageView petAddBtn = viewGroup.findViewById(R.id.profile_petAddBtn);
-        final ImageView profileImage = viewGroup.findViewById(R.id.profile_image);
-        final TextView userName = viewGroup.findViewById(R.id.profile_name);
-        final TextView userMemo = viewGroup.findViewById(R.id.profile_memo);
 
 
         // 프로필 이미지 수정
         profileImage.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-              //  setImg();
+                //  setImg();
                 setProfileImg(profileImgName);
                 String userId = "IAmTarget";//"IAmUser"
                 String targetId = "IAmTarget";
@@ -173,7 +142,7 @@ public class FragmentMy extends Fragment {
                 intent.putExtra("targetId", targetId);
                 intent.putExtra("userId", userId);
                 intent.putExtra("userImage", profileImgName);// userInfo.get("profileImg")); // 임시로 넣은 이미지
-                intent.putExtra("userName",   profileName.getText().toString());// userInfo.get("nickName"));//userName.getText().toString());
+                intent.putExtra("userName", profileName.getText().toString());// userInfo.get("nickName"));//userName.getText().toString());
                 intent.putExtra("userMemo", profileMemo.getText().toString());//userInfo.get("memo"));//userMemo.getText().toString());
 
                 startActivityForResult(intent, 0);
@@ -191,6 +160,44 @@ public class FragmentMy extends Fragment {
         });
 
         return viewGroup;
+    }
+
+
+    // 개인 게시물 로드
+    private void loadPosts()
+    {
+        //  유저
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Query query = db.collection("post").whereEqualTo("uid", uid);
+        //query.get
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                Data dataList = new Data();
+                                dataList.setUid(document.getData().get("uid").toString());
+                                dataList.setContent(document.getData().get("content").toString());
+                                dataList.setImageUrl1(document.getData().get("imageUrl1").toString());
+                                dataList.setImageUrl2(document.getData().get("imageUrl2").toString());
+                                dataList.setImageUrl3(document.getData().get("imageUrl3").toString());
+                                dataList.setImageUrl4(document.getData().get("imageUrl4").toString());
+                                dataList.setImageUrl5(document.getData().get("imageUrl5").toString());
+                                dataList.setNickName(document.getData().get("nickName").toString());
+                                postList.add(0, dataList);
+                                Log.d("태그 확인", document.getId() + "dataList => " + dataList.getImageUrl1());
+                            }
+                            adapter.notifyDataSetChanged();
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
     }
 
     private void startToast(String msg) {
@@ -214,13 +221,13 @@ public class FragmentMy extends Fragment {
         switch (requestCode) {
             case 0:
                 if (resultCode == RESULT_OK) {
-                   // userInfo.put("nickName", data.getStringExtra("nickName"));
-                  //  userInfo.put("memo",data.getStringExtra("memo"));
-                  //userInfo.put("profileImg", data.getStringExtra("profileImg"));
+                    // userInfo.put("nickName", data.getStringExtra("nickName"));
+                    //  userInfo.put("memo",data.getStringExtra("memo"));
+                    //userInfo.put("profileImg", data.getStringExtra("profileImg"));
 
                     setProfileImg(data.getStringExtra("profileImg"));
-                    profileName.setText( data.getStringExtra("nickName"));
-                    profileMemo.setText(  data.getStringExtra("memo"));
+                    profileName.setText(data.getStringExtra("nickName"));
+                    profileMemo.setText(data.getStringExtra("memo"));
 
                 } else {
                 }
@@ -253,58 +260,4 @@ public class FragmentMy extends Fragment {
         Glide.with(this).load(profileImg).centerCrop().override(500).into(profileEditImg);
     }
 
-
-    //irang temp -------------------------------------------------------
-
-    public class MypageGridItem {
-        int _imgName;
-
-        public MypageGridItem(int imgName) {
-            _imgName = imgName;
-            Log.d("test", "imgName :  " + imgName);
-        }
-    }
-
-    class GridListAdapter extends BaseAdapter {
-        Context context;
-        ArrayList<MypageGridItem> items = new ArrayList<MypageGridItem>();
-
-        public void addItem(MypageGridItem item) {
-            items.add(item);
-        }
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            context = parent.getContext();
-            MypageGridItem listItem = items.get(position);
-
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.item_mypage_temp, parent, false);
-            }
-
-            //xml의 ImageView 참조
-            ImageView image = convertView.findViewById(R.id.mypage_image);
-
-            image.setImageResource(listItem._imgName);
-
-            return convertView;
-
-        }
-    }
 }
